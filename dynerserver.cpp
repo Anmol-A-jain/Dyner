6e5d061ccd5e8e17b2283ff06200c05a54320ad0
Anmol-A-jain <ajanmol31@gmail.com>
2019-12-19 13:09:44 +0530

    Update Designing

---
+++ dynerserver.cpp
@@ -0,0 +1,6 @@
+#include "dynerserver.h"
+
+DynerServer::DynerServer()
+{
+
+}
---
+++ dynerserver.h
@@ -0,0 +1,16 @@
+#ifndef DYNERSERVER_H
+#define DYNERSERVER_H
+
+
+class DynerServer : public QDeclarativeItem
+{
+    Q_OBJECT
+public:
+    DynerServer();
+
+signals:
+
+public slots:
+};
+
+#endif // DYNERSERVER_H
