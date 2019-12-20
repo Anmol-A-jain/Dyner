2ee36836c0ea3f54633aba18ce5a02c23aa0268b
Anmol-A-jain <ajanmol31@gmail.com>
2019-12-21 01:38:43 +0530

    adding server (multi thread)

---
+++ server/dynerserver.cpp
@@ -0,0 +1,42 @@
+#include "server/dynerserver.h"
+#include "server/mytcpsocket.h"
+
+
+DynerServer::DynerServer(QObject *parent)
+    :QTcpServer(parent)
+{
+
+}
+
+void DynerServer::incomingConnection(qintptr socketDescriptor)
+{
+    // We have a new connection
+    qDebug() << socketDescriptor << " Connecting...";
+
+    MyTcpSocket *thread = new MyTcpSocket(socketDescriptor, this);
+
+    // connect signal/slot
+    // once a thread is not needed, it will be beleted later
+    connect(thread, SIGNAL(finished()), thread, SLOT(deleteLater()));
+
+    thread->start();
+}
+
+void DynerServer::startServer()
+{
+    clientlist = new QVector<MyTcpSocket*>;
+
+    quint16 port(4444);
+
+    if(!this->listen(QHostAddress::Any,port))
+    {
+        qDebug() << "Could not start server";
+    }
+    else
+    {
+        qDebug() << "Listening to port " << port << "...";
+    }
+
+}
+
+
